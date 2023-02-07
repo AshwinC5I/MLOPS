907ebc09d6e7368e5ae75f0f8b1341ff654d2290
Ashwin D <Ashwin.D@course5i.com>
2023-02-07 15:07:58 +0530

    train_and_evaluate

--- src/get_data.py
+++ src/get_data.py
@@ -17,8 +17,7 @@ def get_data(config_path):
 def read_params(config_path):
     with open(config_path) as yaml_file:
         config=yaml.safe_load(yaml_file)
-
-    return config
+        return config
 
 
 
